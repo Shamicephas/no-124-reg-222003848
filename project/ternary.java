19dac4bb52470f177c7b5b2ea8bdf6fced620c92
Shamicephas <149762830+Shamicephas@users.noreply.github.com>
2023-11-06 21:48:14 -0800

    Update ternary.java

--- project/ternary.java
+++ project/ternary.java
@@ -1,7 +1,7 @@
-class example{
-    public static void main(String args[]){
-int a=21;
-int b=24;
-int min=(a<b)?a:b;
-System.out.println(min);
+class example{//class name
+    public static void main(String args[]){//main class
+int a=21;//integer variable a
+int b=24;//integer variable b
+int min=(a<b)?a:b;//if a is less than b print else print b
+System.out.println(min);//output will be variable min
 }}
